a6f5de51a0a098fb758b9dd1c4e2a7a938d41b87
Yahyahassann <yahyahajjhassan00@gmail.com>
2025-10-24 13:33:41 +0300

    Setup the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+
 app.use(express.json());
 mongoose.connect('mongodb://localhost:27017/stocktrack' )
   .then(() => console.log('MongoDB connected'))
